59cad7d015f71acb373d624f50d9ede88c60dd38
Himanshu Mandal <hmandal007@gmail.com>
2015-11-04 00:15:48 +0530

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name minesweeperAngularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the minesweeperAngularApp
+ */
+angular.module('minesweeperAngularApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
